9dc1cea97a8fa317d60e04b02079a799b9174443
jyotiprajapti <jyotiprajapati3029@gmail.com>
2023-01-19 19:35:15 +0530

    master barnch msg displayed

---
+++ LineComparision.java
@@ -0,0 +1,11 @@
+package com.bridgelab;
+import java.util.Scanner;
+public class LineComparision {
+	
+
+
+	    public static void main(String[] args) {
+	      System.out.println("Welcome to line comparision");
+
+}
+}
